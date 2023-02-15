1c7f18bc1805aff07b9979a3e4be38d0ae72bc8c
Rigiih7 <mwirigihbrian@gmail.com>
2023-02-16 00:32:23 +0300

    function fizzbuzz

---
+++ Fizzbuzz-3.py
@@ -0,0 +1,9 @@
+def fizz_buzzz(input):
+    if input%15 == 0:
+        return 'FizzBuzz'
+    if input%3 == 0:
+        return 'Fizz'
+    if input%5 == 0:
+        return 'Buzz'
+    return input
+print(fizz_buzzz(15))
